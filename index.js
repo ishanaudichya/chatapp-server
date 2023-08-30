29002ac72899d602b55a1f88ed2d0d06764dcdb0
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-08-30 23:43:41 +0530

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const CHAT_ENGINE_PROJECT_ID = "21872957-f175-49c0-ba4a-9cf5074093eb";
+const CHAT_ENGINE_PRIVATE_KEY = "fe12f357-6cea-40ce-b6c5-cc798af1aa1b";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3001
+}
+app.listen(port);
